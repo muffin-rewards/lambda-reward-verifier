442d131052f5e3ea504063c33d6f736d6613b311
Michael Bausano <michael@joinpouch.com>
2018-12-01 16:35:02 +0000

    persist entry id change

--- src/persistEntry.js
+++ src/persistEntry.js
@@ -3,15 +3,15 @@ AWS.config.update({ region: 'eu-west-1' })
 const ddb = new AWS.DynamoDB()
 const crypto = require('crypto')
 
-module.exports = (token, id, reward) => {
+module.exports = (token, postId, reward) => {
   const code = crypto.randomBytes(48).toString('hex')
 
   return ddb.putItem({
     Item: {
-      code: { S: code },
+      postId: { S: postId },
       userId: { S: token },
       reward: { S: reward },
-      id: { S: id },
+      id: { S: code },
       used: { BOOL: false }
     },
     TableName: process.env.DDB_ENTRIES_TABLE
